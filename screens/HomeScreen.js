36ae94aace9264514ced59fb96adeeca0e726503
Asaf Kfir <asafkfir11@gmail.com>
2024-06-12 14:09:49 +0300

    feat: test android / ios build

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,7 @@
 // screens/HomeScreen.js
 import React, { useState, useEffect } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from "react-native-auth0";
 
 export default function HomeScreen({ navigation }) {
   const username = "User"; // Replace with actual username
@@ -9,8 +9,8 @@ export default function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <LoginButton />
-      <LogoutButton />
-      <Profile />
+      {/* <LogoutButton />
+      <Profile /> */}
       <Text style={styles.welcomeText}>Hello {username}</Text>
       <Button
         title="Start Answering"
@@ -35,59 +35,73 @@ const styles = StyleSheet.create({
   },
 });
 
+// const LoginButton = () => {
+//   const { loginWithRedirect } = useAuth0();
+
+//   return <button onClick={() => loginWithRedirect()}>Log In</button>;
+// };
+
+// const LogoutButton = () => {
+//   const { logout } = useAuth0();
+
+//   return (
+//     <button
+//       onClick={() =>
+//         logout({ logoutParams: { returnTo: window.location.origin } })
+//       }
+//     >
+//       Log Out
+//     </button>
+//   );
+// };
+
+// const Profile = () => {
+//   const { user, isAuthenticated, isLoading, getAccessTokenSilently } =
+//     useAuth0();
+
+//   if (isLoading) {
+//     return <div>Loading ...</div>;
+//   }
+
+//   const fetchData = async () => {
+//     try {
+//       const token = await getAccessTokenSilently();
+
+//       console.log("Access Token:", token); // Log the token
+//     } catch (error) {
+//       console.error("Error fetching protected data:", error);
+//     }
+//   };
+
+//   if (isAuthenticated) {
+//     try {
+//       fetchData();
+//     } catch (error) {
+//       console.error("Error fetching protected data:", error);
+//     }
+//   }
+
+//   return (
+//     isAuthenticated && (
+//       <div>
+//         <img src={user.picture} alt={user.name} />
+//         <h2>{user.name}</h2>
+//         <p>{user.email}</p>
+//       </div>
+//     )
+//   );
+// };
+
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { authorize } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
-};
-
-const LogoutButton = () => {
-  const { logout } = useAuth0();
-
-  return (
-    <button
-      onClick={() =>
-        logout({ logoutParams: { returnTo: window.location.origin } })
-      }
-    >
-      Log Out
-    </button>
-  );
-};
-
-const Profile = () => {
-  const { user, isAuthenticated, isLoading, getAccessTokenSilently } =
-    useAuth0();
-
-  if (isLoading) {
-    return <div>Loading ...</div>;
-  }
-
-  const fetchData = async () => {
+  const onPress = async () => {
     try {
-      const token = await getAccessTokenSilently();
-
-      console.log("Access Token:", token); // Log the token
-    } catch (error) {
-      console.error("Error fetching protected data:", error);
+      await authorize();
+    } catch (e) {
+      console.log(e);
     }
   };
 
-  if (isAuthenticated) {
-    try {
-      fetchData();
-    } catch (error) {
-      console.error("Error fetching protected data:", error);
-    }
-  }
-
-  return (
-    isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      </div>
-    )
-  );
+  return <Button onPress={onPress} title="Log in" />;
 };
